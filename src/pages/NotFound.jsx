a9a40839d391184fa86b99a3abecbdf2328b5c9e
Dostonbek <dostonbek200gmail.com>
2025-02-16 20:56:21 +0500

    npt

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,17 @@
 import NavLinks from "../components/NavLinks";
 
 function NotFound() {
-  return <NavLinks />;
+  return (
+      <div className="flex items-center justify-center h-screen flex-col gap-5">
+      <h1 className="text-6xl font-bold text-red-800">404</h1>
+      <p className="text-3xl">Afsus bunday sahifa mavjud emas</p>
+      <button className="btn btn-outline btn-primary">
+        <NavLinks to={"/"} className={"text-[#000000]"}>
+          Bosh sahifaga qaytish
+        </NavLinks>
+      </button>
+    </div>
+  )
 }
 
 export default NotFound;
